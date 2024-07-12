c95c1ed018a358e31e2cddb10ca3bef59e8df6f6
晴天雨露 <clovme@163.com>
2024-07-13 04:48:48 +0800

    完成菜单接口

--- controllers/menus/put.go
+++ controllers/menus/put.go
@@ -1,7 +1,79 @@
 package menus
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"resume/libs"
+	"resume/models"
+)
 
-func Put(c *gin.Context) {
+// SwitchStatus 切换菜单状态
+func SwitchStatus(c *gin.Context) {
+	s := libs.Context(c)
 
+	var menu models.Menus
+	if err := c.ShouldBind(&menu); err != nil {
+		log.Println("传入的菜单数据反序列化失败！")
+		s.Msg(http.StatusBadRequest, "排序失败，请重试！")
+		return
+	}
+	result := s.Model(&menu).Where("id = ? AND uid =  ? AND rid = ?", menu.ID, s.User.ID, s.Resume.ID).Update("is_checked", menu.IsChecked)
+	// 检查更新是否成功
+	if result.RowsAffected > 0 {
+		s.Msg(http.StatusOK, "状态更新成功！")
+		return
+	}
+	s.Msg(http.StatusBadRequest, "状态更新失败！")
+}
+
+// EditName 修改模块名称
+func EditName(c *gin.Context) {
+	s := libs.Context(c)
+
+	var menu models.Menus
+	if err := c.ShouldBind(&menu); err != nil {
+		log.Println("传入的菜单数据反序列化失败！")
+		s.Msg(http.StatusBadRequest, "排序失败，请重试！")
+		return
+	}
+
+	if err := s.First(&menu, "uid = ? AND rid = ? AND title = ?", s.User.ID, s.Resume.ID, menu.Title).Error; err == nil {
+		s.Msg(http.StatusBadRequest, "模块名相同，不能修改！")
+		return
+	}
+
+	if result := s.Model(&menu).Where("uid = ? AND rid = ?", s.User.ID, s.Resume.ID).Update("title", menu.Title); result.Error != nil {
+		log.Println("模块名称更新失败！", result.Error)
+		s.Msg(http.StatusBadRequest, "模块名称更新失败！")
+		return
+	}
+	s.Msg(http.StatusOK, "模块名称更新成功！")
+}
+
+// Sort 排序
+func Sort(c *gin.Context) {
+	s := libs.Context(c)
+
+	var menus []models.Menus
+	if err := c.ShouldBind(&menus); err != nil {
+		log.Println("传入的菜单数据反序列化失败！")
+		s.Msg(http.StatusBadRequest, "排序失败，请重试！")
+		return
+	}
+
+	success := 0
+	libs.Transaction(s, func(tx *gorm.DB) {
+		for _, menu := range menus {
+			result := tx.Model(&menu).Where("id = ? AND uid = ? AND rid = ?", menu.ID, s.User.ID, s.Resume.ID).Update("sort", menu.Sort)
+			// 检查更新是否成功
+			if result.RowsAffected > 0 {
+				success++
+			}
+		}
+	})
+
+	s.Msg(http.StatusOK, fmt.Sprintf("排序完成%d/%d条!", success, len(menus)))
 }
--- routers/menus.go
+++ routers/menus.go
@@ -4,7 +4,12 @@ import (
 	"resume/controllers/menus"
 )
 
-func (router *Routers) Menus() {
+func (router *Routers) MenusGet() {
 	router.Protected.GET("/menus", menus.Get)
-	router.Protected.PUT("/menus", menus.Put)
+}
+
+func (router *Routers) MenusPut() {
+	router.Protected.PUT("/menus/sort", menus.Sort)
+	router.Protected.PUT("/menus/edit/name", menus.EditName)
+	router.Protected.PUT("/menus/switch/status", menus.SwitchStatus)
 }
