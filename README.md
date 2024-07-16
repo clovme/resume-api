bc80fc3889b914e8e073d548821f01aa3339d2e0
晴天雨露 <clovme@163.com>
2024-07-16 12:28:45 +0800

    设置选项默认值

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # resume
 个人简历API接口
+
+go version go1.22.2 windows/amd64
--- database/initdata/basicinfo.go
+++ database/initdata/basicinfo.go
@@ -8,7 +8,7 @@ import (
 // BasicInfo 初始化基础信息表
 func (d *InitData) BasicInfo() {
 	modelList := []models.Basicinfo{
-		{BaseModelWithRIDUID: ridUID(), IsAge: true},
+		{BaseModelWithRIDUID: ridUID(), IsAge: true, Gender: "不填", MaritalStatus: "不填", PoliticalStatus: "不填", WorkExperienceYears: "不填", StartDate: "不填"},
 	}
 	insertRecord[models.Basicinfo]("基础信息", modelList, func(model models.Basicinfo) (db, where *gorm.DB) {
 		return d.Db, d.Db.Where("rid = ? and uid = ?", model.RID, model.UID)
