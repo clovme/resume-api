66414eaf2b508b0f7cbb637e3ba69c35828913d3
晴天雨露 <clovme@163.com>
2024-07-19 16:54:18 +0800

    添加配置文件生成

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"path/filepath"
 	"resume/database"
 	"resume/libs"
 	"resume/routers"
+	"strings"
 )
 
 //go:embed public/*
@@ -20,6 +23,22 @@ func main() {
 	var dsn gorm.Dialector
 
 	libs.CreateDir("data")
+
+	if len(os.Args) == 2 && strings.ToLower(os.Args[1]) == "init" {
+		file, err := static.ReadFile("public/config.ini.tpl")
+		if err != nil {
+			return
+		}
+		os.WriteFile("data/config.ini", file, 0644)
+		log.Println("配置文件已生成！")
+		os.Exit(0)
+		return
+	}
+
+	fmt.Println("============================================================================")
+	fmt.Println(fmt.Sprintf("= %s init 生成配置文件。", filepath.Base(os.Args[0])))
+	fmt.Println("============================================================================")
+
 	libs.CreateDir("data/temp")
 
 	// 读取 INI 文件
