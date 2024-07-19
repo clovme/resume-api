db543b7b089bb896d9c8c2e27a56c7ce1241fdf9
晴天雨露 <clovme@163.com>
2024-07-19 08:59:09 +0800

    分离web服务

---
+++ web.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"resume/database"
+	"resume/libs"
+	"resume/routers"
+)
+
+func runWeb() {
+	var dsn gorm.Dialector
+
+	libs.CreateDir("data")
+	libs.CreateDir("data/temp")
+
+	// 读取 INI 文件
+	cfg, err := ini.Load("data/config.ini")
+	if err != nil {
+		cfg = ini.Empty()
+		cfg.Section("server").Key("host").SetValue("127.0.0.1")
+		cfg.Section("server").Key("port").SetValue("8080")
+		dsn = sqlite.Open("data/resume.db")
+		log.Println("读取 INI 配置文件打开失败，使用默认配置，错误信息：", err)
+	} else {
+		dsn = database.OpenMySQLDB(cfg)
+	}
+
+	// 初始化数据库
+	db := database.AutoMigrate(dsn)
+
+	router := gin.Default()
+	// 视图路由配置
+	routers.Initialization(router, db, &static)
+
+	host := cfg.Section("server").Key("host").String()
+	port := cfg.Section("server").Key("port").String()
+	err = router.Run(fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		log.Fatalf("WEB 服务器启动失败: %v", err)
+	}
+}
